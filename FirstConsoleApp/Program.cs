9df34eca5ce75ea77b528a45f3807c17ea27b547
Alex <gadda023@gmail.com>
2022-05-29 12:47:29 +1200

    nothing notable yet

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -1,4 +1,10 @@
-﻿namespace FirstConsoleApp
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp
 {
     class Program
     {
